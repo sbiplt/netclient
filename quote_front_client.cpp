d038b7bd8a1d58a25fcf77e228a91d240a84fe9a
lxj <sbiplt@126.com>
2021-05-15 23:18:56 +0800

    fix

--- quote_front_client.cpp
+++ quote_front_client.cpp
@@ -9,62 +9,30 @@
 #include <boost/shared_ptr.hpp>
 #include <asio.hpp>
 #include <asio/io_service.hpp>
+#include <boost/circular_buffer.hpp>
 #include <set>
 
-#define PRINT_BUFF_COUNT 10000
+#define MAX_SOCKET 5000
 #define BUF_SIZE 8000
 #define CONNECT_PER_LOOP 200
 
-unsigned int max_fds = 2000;
+unsigned int max_fds = MAX_SOCKET;
+std::string sub_msg{ "sub_msg" };
+std::string unsub_msg{ "unsub_msg" };
+std::string heartbeat_msg{ "heartbeat_msg" };
 
 int log_fd = -1;
 char svr_ip[32] = "192.168.1.229";
 int port = 11700;
 
-boost::lockfree::spsc_queue<char*, 
-        boost::lockfree::capacity<PRINT_BUFF_COUNT> , 
-        boost::lockfree::fixed_sized<true> > print_buff;
-
-boost::lockfree::spsc_queue<char*, 
-        boost::lockfree::capacity<PRINT_BUFF_COUNT> , 
-        boost::lockfree::fixed_sized<true> > print_data;
-
-void handle_sig(int signum)
-{
-    printf("receiv sig int");
-    exit(0);
-}
-class event_handler;
-
-void print_handler()
-{
-	for (int i = 0; i < PRINT_BUFF_COUNT; i++)
-	{
-		print_buff.push(new char[BUF_SIZE]);
-	}
-
-	boost::thread th(
-		[] {
-		char *buf;
-		while (true) {
-		
-			if (print_data.pop(buf))
-			{
-				if (*buf)
-				{
-					std::cout << "recv: " << buf << std::endl;;
-				}
-				print_buff.push(buf);
-			}
-		}
-	});
-	th.join();
-}
+boost::lockfree::spsc_queue<boost::shared_ptr<void>,
+	boost::lockfree::capacity<MAX_SOCKET>,
+	boost::lockfree::fixed_sized<true> > socket_del_queue;
 
 class tcp_channel : public boost::enable_shared_from_this<tcp_channel>
 {
 public:
-	tcp_channel(asio::io_service &io_):socket_(io_),ep(asio::ip::address::from_string(svr_ip), port),timer(io_, std::chrono::seconds(10))
+	tcp_channel(asio::io_service &io_):ioc(io_),socket_(io_),ep(asio::ip::address::from_string(svr_ip), port),timer(io_, std::chrono::seconds(10))
 	{
 		timer.async_wait(std::bind(&tcp_channel::send_beat, this, std::placeholders::_1));
 	}
@@ -73,12 +41,33 @@ public:
 		socket_.close();
 		std::cout << "channel release ..." << std::endl;
 	}
-
-	void start(std::function<void()> f, bool need_print = false)
+	
+	void release()
 	{
-		this->need_print = need_print;
-		exit_ = f;
-		do_connect();
+		timer.cancel();
+		auto self(shared_from_this());
+		ioc.post([self] {socket_del_queue.push(self); });
+	}
+
+	void send_beat(std::error_code ec)
+	{
+		if (!ec)
+		{
+			write_buf_queue.push_back(heartbeat_msg);
+			if (!is_writing)
+			{
+				is_writing = true;
+				do_write();
+			}
+			timer.expires_at(timer.expiry() + asio::chrono::seconds(2));
+			auto self(shared_from_this());
+			timer.async_wait(std::bind(&tcp_channel::send_beat, self, std::placeholders::_1));
+		}
+		else
+		{
+			printf("time error: Errcode=%d,ErrMsg=%s\n", ec.value(), ec.message().c_str());
+			release();
+		}
 	}
 
 	void do_connect()
@@ -89,16 +78,9 @@ public:
 		{
 			if (!ec)
 			{
-				do_write(sub_msg);
-
-				if(!need_print)
-				{
-					do_read();
-				}
-				else
-				{
-					do_read_print();
-				}
+				do_read();
+				write_buf_queue.push_back(sub_msg);
+				do_write();
 			}
 			else
 			{
@@ -108,30 +90,26 @@ public:
 		});
 	}
 
-    void send_beat(std::error_code ec)
-    {
-		if (!ec) 
-		{
-			do_write(heartbeat_msg);
-			timer.expires_at(timer.expiry() + asio::chrono::seconds(10));
-			auto self(shared_from_this());
-			timer.async_wait(std::bind(&tcp_channel::send_beat, self, std::placeholders::_1));
-		}
-		else
-		{
-			printf("time error: Errcode=%d,ErrMsg=%s\n", ec.value(), ec.message().c_str());
-			release();
-		}
-    }		
-
-	void do_write(const std::string& msg)
+	void do_write()
 	{
 		auto self(shared_from_this());
 		asio::async_write(socket_,
-			asio::buffer(msg.c_str(), msg.length()),
+			asio::buffer(write_buf_queue.front()),
 			[this, self](std::error_code ec, std::size_t length)
 		{
-			if (ec)
+			if (!ec)
+			{
+				write_buf_queue.pop_front();
+				if (write_buf_queue.size() > 0)
+				{
+					do_write();
+				}
+				else
+				{
+					is_writing = false;
+				}
+			}
+			else
 			{
 				printf("do_write Disconnect:PeerPort=%d,Errcode=%d,ErrMsg=%s\n", socket_.remote_endpoint().port(), ec.value(), ec.message().c_str());
 				release();
@@ -139,16 +117,10 @@ public:
 		});
 	}
 
-    void release()
-    {
-        timer.cancel();
-        exit_();
-    }
-
 	void do_read()
 	{
 		auto self(shared_from_this());
-		socket_.async_read_some(asio::buffer(buf, BUF_SIZE),
+		socket_.async_read_some(asio::buffer(read_buf, BUF_SIZE),
 			[this, self](std::error_code ec, std::size_t bytes)
 		{
 			if (!ec)
@@ -163,77 +135,42 @@ public:
 		});
 	}
 
-	void do_read_print()
-	{
-		char *buff;
-	    bool is_print = print_buff.pop(buff);
-		if (is_print)
-		{
-			buff = buf;
-			std::cout << "print buff exhaust " << print_data.write_available() << std::endl;
-		}
-		auto self(shared_from_this());
-		socket_.async_read_some(asio::buffer(buff, BUF_SIZE),
-			[this, self, buff, is_print](std::error_code ec, std::size_t bytes)
-		{
-			if (!ec)
-			{
-				if (is_print)
-				{
-					print_data.push(buff);
-				}
-				do_read_print();
-			}
-			else
-			{
-				printf("do_read Disconnect:PeerPort=%d,Errcode=%d,ErrMsg=%s\n", socket_.remote_endpoint().port(), ec.value(), ec.message().c_str());
-				release();
-			}
-		});
-	}
+	char read_buf[BUF_SIZE + 1];
+	boost::circular_buffer<std::string> write_buf_queue{ 100 };
 
-	char buf[BUF_SIZE + 1];
+	asio::io_context &ioc;
 	asio::ip::tcp::socket socket_;
-	boost::function<void()> exit_;
 	asio::ip::tcp::endpoint ep;
-	bool need_print;
+
+	bool is_writing{false};
 
     asio::steady_timer timer; 
-	std::string sub_msg{ "{(len=29)MARKET01@@S@+@@@&NASD,AAPL.US}" };
-	std::string unsub_msg{ "{(len=29)MARKET01@@S@-@@@&NASD,AAPL.US}" };
-	std::string heartbeat_msg{ "{(len=19)TEST0001@@@@@@@@@@&}" };
 };
 
 class event_handler 
 {
 public:
-	event_handler() 
-	{ 
+	void start()
+	{
 		boost::thread th1([this]
 		{
-			asio::io_context::work worker(context);  
+			asio::io_context::work worker(context);
 			context.run();
 		});
-		th1.detach();
 
 		boost::thread th2([this] {
-			while (true)
+			boost::shared_ptr<void> ptr;
+			while (is_running)
 			{
+				while (socket_del_queue.pop(ptr))
+				{
+					channel_set.erase(ptr);
+				}
 				if (channel_set.size() < max_fds)
 				{
 					auto ptr = boost::make_shared<tcp_channel>(context);
 					channel_set.insert(ptr);
-					bool is_print = log_socket == nullptr;
-					ptr->start([this, ptr] {
-						if (log_socket == ptr) {
-							log_socket = nullptr;
-						}
-						channel_set.erase(ptr); 
-					}, is_print);
-					if (is_print)
-					{
-						log_socket = ptr;
-					}
+					ptr->do_connect();
 				}
 				else
 				{
@@ -241,23 +178,22 @@ public:
 				}
 			}
 		});
-		th2.detach();
-	}
-
-	~event_handler() 
-	{ 
-		context.stop();
+		th2.join();
 	}
     
+	void stop()
+	{
+		is_running = false;
+	}
+
 private:
+	bool is_running{true};
 	asio::io_service context;
-	boost::shared_ptr<void> log_socket;
 	std::set<boost::shared_ptr<void>> channel_set;
 };
 
 int main(int argc, char *argv[]) {
-    signal(SIGINT, handle_sig);
-    
+
     if(argc > 1)
     {
         max_fds = std::stoi(argv[1]);
@@ -278,8 +214,13 @@ int main(int argc, char *argv[]) {
 
 	event_handler eh;
 
-    print_handler();
+	signal(SIGINT, [](int signum)
+	{
+		printf("receiv sig int");
+		exit(0);
+	});
 
+	eh.start();
     return 0;
 }
 
